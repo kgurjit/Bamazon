57dd19b9845e5e495eea2da957b4f7e4e4a945b9
Gurjit Kaur <kgurjit@gmail.com>
2016-07-24 20:40:38 -0400

    Fixed messages

--- BamazonCustomer.js
+++ BamazonCustomer.js
@@ -9,7 +9,11 @@ var connection = mysql.createConnection({
 });
 connection.connect();
 
-var showProductsAndPromtUser = function(){
+var showProductsAndPromtUser = function(onlyPrompt, existingData){
+	if(onlyPrompt) {
+		promptUser(existingData);
+		return;
+	}
 	connection.query('select * from Products', function(err, rows, fields) {
   		if (err) throw err;
   		console.log('ItemID\tPdt Name\tPrice\tStock');
@@ -37,13 +41,14 @@ var promptUser = function(data){
 					} else {
 						var details = data[choices.ItemID];
 						if(choices.qty >= details.StockQuantity) {
-							console.log("We're out of stock on that product. Try another product or quantity");
-							showProductsAndPromtUser();
+							console.log("Insufficient quantity.");
+							showProductsAndPromtUser(true, data);
 						} else {
+							var totalCost = choices.qty * details.Price;
 							var query = "update Products set StockQuantity = StockQuantity - " + choices.qty + " where ItemID=" + choices.ItemID;
 							connection.query(query, function(err, rows, fields) {
 						  		if (err) throw err;
-						  		console.log('Purchase complete');
+						  		console.log('Purchase complete. Your total cost is: ' + totalCost);
 						  		showProductsAndPromtUser();
 							});
 						}
